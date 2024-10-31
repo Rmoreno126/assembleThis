ecc0b90c5ca10f8c7cd63cc77f7b08751c29e240
Ricardo M <95rikym@gmail.com>
2024-10-31 15:38:42 -0700

    New Project

---
+++ src/test/java/edu/redwoods/cis18/assemble/AssembleApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.redwoods.cis18.assemble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssembleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
