febccec2b89509c2e3e74c484d57f80f2a232a1d
Ricardo M <95rikym@gmail.com>
2024-10-31 16:29:07 -0700

    updated application.properties

---
+++ src/main/java/edu/redwoods/cis18/assemble/model/User.java
@@ -0,0 +1,58 @@
+package edu.redwoods.cis18.assemble.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // This method is overridden so that the DATABASE record id is used for comparing object equality
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id);
+    }
+
+    // This method is overridden so that the object id will be tied to the DATABASE record id.
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
