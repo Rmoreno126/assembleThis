b147f44b0774d2fb0d878dda8bd5797b0a7f2e3d
Ricardo M <95rikym@gmail.com>
2024-12-03 12:52:25 -0800

    updated BusinessService.java and pom.xml file

--- src/main/java/edu/redwoods/cis18/assemble/service/BusinessService.java
+++ src/main/java/edu/redwoods/cis18/assemble/service/BusinessService.java
@@ -1,9 +1,11 @@
 package edu.redwoods.cis18.assemble.service;
 
 import edu.redwoods.cis18.assemble.model.Business;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class BusinessService {
     public List<Business> getAllBusinesses() {
         return List.of();
